acb4aaeef23f9765074e3ae4471361ebe914d733
Vihar <89672633+WorkingAtGoogle@users.noreply.github.com>
2024-02-24 15:09:45 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     # Needed to actually package something
     packages=['scratchie'],
     # Needed for dependencies
-    install_requires=dynamic,
+    install_requires=['pyttsx3'],
     # *strongly* suggested for sharing
     version='0.1',
     # The license can be anything you like
