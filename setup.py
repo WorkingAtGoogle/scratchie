237752221c79c7e8591d10735e1ed23f81dbdfc1
Vihar <89672633+WorkingAtGoogle@users.noreply.github.com>
2024-02-24 15:05:04 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,11 +9,11 @@ setup(
     # Needed to actually package something
     packages=['scratchie'],
     # Needed for dependencies
-    dynamic=['pyttsx3'],
+    install_requires=['pyttsx3'],
     # *strongly* suggested for sharing
     version='0.1',
     # The license can be anything you like
-    license='CC0 1.0 Universal',
+    license='dynamic',
     description='Scratchie is an amazing library for when converting from scratch to python. It has similar syntax to scrath in order to help kids to convert to text-based programming quickly.',
     # We will also need a readme eventually (there will be a warning)
     # long_description=open('README.md').read(),
